2ad26ee67ec2be6f73a65981c66fcaf7c11d0fa7
KarrieD <karriedbrooks@gmail.com>
2022-06-21 13:31:25 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Karrie Brooks and is {" "}
+        <a
+          href="https://github.com/KarrieD/react-weather-project"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.
+      </footer>
     </div>
   );
 }
