5431e29a4df3da6f644582aeac6a0174dc086136
KarrieD <karriedbrooks@gmail.com>
2022-06-27 13:21:39 -0500

    Added Axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,12 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const apiKey = "210412f30f491d1c32937a20641fbe44";
+  let city = "Lubbock";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
   return (
     <div className="Weather">
       <form>
@@ -11,10 +16,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,13 +34,17 @@ export default function Weather() {
         <li>Tuesday 02:15</li>
         <li>Partly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Partly cloudy"
-          ></img>
-          37°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly cloudy"
+              className="float-left"
+            />
+            <span className="temperature">37</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
