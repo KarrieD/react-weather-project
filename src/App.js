c9b6daf29b48086c39b0cee76142f4b5e8cca919
KarrieD <karriedbrooks@gmail.com>
2022-06-27 13:21:55 -0500

    Edited footer

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
-      <footer>
-        This project was coded by Karrie Brooks and is {" "}
-        <a
-          href="https://github.com/KarrieD/react-weather-project"
-          target="_blank"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub.
-      </footer>
+        <Weather />
+        <footer>
+          This project was coded by Karrie Brooks and is {" "}
+          <a
+            href="https://github.com/KarrieD/react-weather-project"
+            target="_blank"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub.
+        </footer>
       </div>
     </div>
   );
